22767184a1e9b996d3c04ec4e57a58d2abd0b164
HarshChauhan2601 <144214795+HarshChauhan2601@users.noreply.github.com>
2024-02-14 22:26:24 +0530

    Update script.js

--- assignment4-solution-easier/script.js
+++ assignment4-solution-easier/script.js
@@ -1,1 +1,10 @@
-
+var arr = new Array("harsh","Jivan", "sivan", "Jarnail");
+for(i=0; i<arr.length; i++){
+    for(j=0; j<i; j++){
+        if((arr[i][j] == "J") || (arr[i][j] == "j")){
+            console.log("GoodBye ", arr[i])
+        }else{
+            console.log("Hello ", arr[i])
+        }
+    }
+}
